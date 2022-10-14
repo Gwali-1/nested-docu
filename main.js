f7465d1ae95c0e8e801439fbd18993ea0642e45e
Gwali <gwalisam37@gmail.com>
2022-10-14 00:43:17 +0000

    added addNestedDocuById method that takes an object of options and returns an error first callback containing error or response depending on operation success

--- main.js
+++ main.js
@@ -23,3 +23,43 @@ const createModel  = function(fields,name){
 
 }
 
+
+const addNestedDocuById = function(options,fn){
+    const fields = ["model","id",'docu',"docuField"];
+    for(const key of fields){
+        if(!options.hasOwnProperty(key)){
+            throw(new Error(`${key} field not indicated`));
+        }
+        
+        if(options[key] === undefined){
+            throw(new Error(`no value for '${key}' key specified`));
+            
+        }
+    }
+
+
+    const {model,id,docu,docuField} = options;
+
+
+    model.findById(id,function(err,returned){
+        if(returned.docuField){
+            model.findByIdAndUpdate(id,{"$push":{docuField:docu}},(err,response) => {
+                if(err){
+                 return fn(err,null)
+                }
+                return fn(null,response)
+            })
+        }
+
+        model.findByIdAndUpdate(id,{docuField:docu},(err,response) => {
+            if(err){
+                return fn(err,null);
+            }
+            fn(null, response);
+        })
+    })
+    ;
+
+}
+
+
